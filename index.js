d5cc3ebae77c77b34b47cdbbafd34e7aca7bd65c
Brian Lawi Ochiel <lawioochiel@gmail.com>
2023-10-15 16:20:47 +0300

    completed assignment

--- index.js
+++ index.js
@@ -1,1 +1,13 @@
 // Code your solution here
+
+function findMatching(driversArr,string){
+ return driversArr.filter(item=>item.toLowerCase() ===  string.toLowerCase());
+}
+
+function fuzzyMatch(driversArr,string){
+    return driversArr.filter(item => item.charAt(0) === string.charAt(0));
+}
+
+function matchName(driversArr,string){
+    return driversArr.filter(item => item.name === string);
+}
